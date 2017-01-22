46df316143a1ca4d05218e24e539e6f73d4db558
sss1 <sss1@andrew.cmu.edu>
2017-01-21 20:30:33 -0500

    Setup for running multiple trials.

--- src/SORN/Sim.java
+++ src/SORN/Sim.java
@@ -13,15 +13,15 @@ import java.util.Random;
  */
 public class Sim {
 
-  private static final int numTrials = 1; // Number of IID trials to run
+  private static final int numTrials = 10; // Number of IID trials to run
   private static final int numNeurons = 200; // Number of neurons
   private static final int duration = 1000; // Number of time steps to simulate
 
   // Parameters specifying where and what the simulation should output
-  private static final boolean makePlot = true;
+  private static final boolean makePlot = false;
   private static final boolean makeMATFile = true;
   private static final String plotFilePath = "/home/sss1/Desktop/SORN/basic_" + numNeurons + "neurons_" + duration + "timesteps.png";
-  private static final String MATFilePath = "/home/sss1/Desktop/SORN/resultsMemory" + Neuron.MEMORY + ".mat";
+  private static final String MATFileRoot = "/home/sss1/Desktop/SORN/resultsMemory" + Neuron.MEMORY;
 
   public static void main(String[] args) {
 
@@ -42,7 +42,8 @@ public class Sim {
     ArrayList<XYSeries> resultsByTrial = new ArrayList<>();
 
     for (int trialIdx = 0; trialIdx < numTrials; trialIdx++) {
-      resultsByTrial.add(runTrial());
+      System.out.println("Starting trial " + (trialIdx + 1) + "/" + numTrials + " of " + label + " experiment.");
+      resultsByTrial.add(runTrial("Trial" + trialIdx));
       System.out.println("Finished trial " + (trialIdx + 1) + "/" + numTrials + " of " + label + " experiment.");
     }
     return Plotter.averageTrials(resultsByTrial, label);
@@ -54,7 +55,7 @@ public class Sim {
    * @return XYSeries each X-value is a time between 0.0 and simDuration and each Y-value is some
    *         quantity of interest
    */
-  private static XYSeries runTrial() {
+  private static XYSeries runTrial(String trialLabel) {
 
     // for s < t, fired[s][i] is true if and only if neuron i fired at time s
     boolean[][] fired = new boolean[duration][numNeurons];
@@ -97,27 +98,29 @@ public class Sim {
 
     }
 
-    int numZeroPairs = 0;
-    int numPartZeroPairs = 0;
-    int numTotalPairs = 0;
-    for (int destNeuronIdx = 0; destNeuronIdx < numNeurons; destNeuronIdx++) {
-      for (int sourceNeuronIdx = 0; sourceNeuronIdx < destNeuronIdx; sourceNeuronIdx++) {
-        output.add(neurons[destNeuronIdx].getWeightIn(sourceNeuronIdx), neurons[sourceNeuronIdx].getWeightIn(destNeuronIdx));
-        numTotalPairs++;
-        if (neurons[destNeuronIdx].getWeightIn(sourceNeuronIdx) < Double.MIN_VALUE || neurons[sourceNeuronIdx].getWeightIn(destNeuronIdx) < Double.MIN_VALUE) {
-          numPartZeroPairs++;
-        }
-        if (neurons[destNeuronIdx].getWeightIn(sourceNeuronIdx) < Double.MIN_VALUE && neurons[sourceNeuronIdx].getWeightIn(destNeuronIdx) < Double.MIN_VALUE) {
-          numZeroPairs++;
-        }
-      }
-    }
-    System.out.println("Fraction of edges that are zero in both directions: " + ((double) numZeroPairs) / numTotalPairs);
-    System.out.println("Fraction of pairs that are connected in at least one direction: " + ((double) (numTotalPairs - numZeroPairs)) / numTotalPairs);
-    System.out.println("Fraction of pairs that are zero in at least one direction: " + ((double) numPartZeroPairs) / numTotalPairs);
-    System.out.println("Fraction of pairs that are connected in both directions: " + ((double) (numTotalPairs - numPartZeroPairs)) / numTotalPairs);
+    // Prints some basic diagnostic connectivity statistics from the simulation
+//    int numZeroPairs = 0;
+//    int numPartZeroPairs = 0;
+//    int numTotalPairs = 0;
+//    for (int destNeuronIdx = 0; destNeuronIdx < numNeurons; destNeuronIdx++) {
+//      for (int sourceNeuronIdx = 0; sourceNeuronIdx < destNeuronIdx; sourceNeuronIdx++) {
+//        output.add(neurons[destNeuronIdx].getWeightIn(sourceNeuronIdx), neurons[sourceNeuronIdx].getWeightIn(destNeuronIdx));
+//        numTotalPairs++;
+//        if (neurons[destNeuronIdx].getWeightIn(sourceNeuronIdx) < Double.MIN_VALUE || neurons[sourceNeuronIdx].getWeightIn(destNeuronIdx) < Double.MIN_VALUE) {
+//          numPartZeroPairs++;
+//        }
+//        if (neurons[destNeuronIdx].getWeightIn(sourceNeuronIdx) < Double.MIN_VALUE && neurons[sourceNeuronIdx].getWeightIn(destNeuronIdx) < Double.MIN_VALUE) {
+//          numZeroPairs++;
+//        }
+//      }
+//    }
+//    System.out.println("Fraction of edges that are zero in both directions: " + ((double) numZeroPairs) / numTotalPairs);
+//    System.out.println("Fraction of pairs that are connected in at least one direction: " + ((double) (numTotalPairs - numZeroPairs)) / numTotalPairs);
+//    System.out.println("Fraction of pairs that are zero in at least one direction: " + ((double) numPartZeroPairs) / numTotalPairs);
+//    System.out.println("Fraction of pairs that are connected in both directions: " + ((double) (numTotalPairs - numPartZeroPairs)) / numTotalPairs);
 
     if (makeMATFile) { // output simulation results to .mat file
+      String MATFilePath = MATFileRoot + trialLabel + ".mat";
       System.out.println("Writing MATLAB output to " + MATFilePath);
       MatPlotter matPlotter = new MatPlotter();
       matPlotter.writeToMAT(MATFilePath, fired, weights, numNeurons, duration);
